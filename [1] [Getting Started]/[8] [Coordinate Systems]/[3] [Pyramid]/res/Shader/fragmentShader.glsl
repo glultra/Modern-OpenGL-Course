b90417393161c6a12f2e27c1ca8a3b0907a70978
Mohammed Khalid <77267002+glULTRA@users.noreply.github.com>
2021-08-12 20:28:59 +0300

    Add files via upload

---
+++ [1] [Getting Started]/[8] [Coordinate Systems]/[3] [Pyramid]/res/Shader/fragmentShader.glsl
@@ -0,0 +1,19 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 TexCoords;
+in vec3 ourColor;
+
+uniform vec3 colors;
+
+uniform sampler2D container_texture;
+uniform sampler2D face_texture;
+uniform sampler2D pyramid_texture;
+
+uniform float alpha;
+
+void main()
+{
+	FragColor = texture(pyramid_texture, TexCoords);
+}
