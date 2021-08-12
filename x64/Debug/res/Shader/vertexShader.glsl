7ae25e740fd7717ba0b3b5a958121f1748237dd1
7amakurd <77267002+7amakurd@users.noreply.github.com>
2021-08-12 03:28:20 +0300

    Add files via upload

---
+++ x64/Debug/res/Shader/vertexShader.glsl
@@ -0,0 +1,21 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+out vec2 TexCoords;
+out vec3 Normal;
+out vec3 FragPos;
+
+uniform mat4 projection;
+uniform mat4 model;
+uniform mat4 view;
+
+void main()
+{
+	Normal = mat3(transpose(inverse(model))) * aNormal;
+	TexCoords = aTexCoords;
+	FragPos = vec3(model * vec4(aPos,1.0f));
+	gl_Position = projection * view * model  * vec4(aPos, 1.0f);
+}
