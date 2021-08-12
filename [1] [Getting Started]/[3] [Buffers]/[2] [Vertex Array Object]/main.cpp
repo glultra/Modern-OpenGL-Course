8633ba81869122e9cb23488dd55049f54b255904
Mohammed Khalid <77267002+glULTRA@users.noreply.github.com>
2021-08-12 15:18:34 +0300

    Update main.cpp

--- [1] [Getting Started]/[3] [Buffers]/[2] [Vertex Array Object]/main.cpp
+++ [1] [Getting Started]/[3] [Buffers]/[2] [Vertex Array Object]/main.cpp
@@ -7,7 +7,7 @@ float vertices[] =
 	/* Position */
 	-0.5f, -0.5f, 0.0f, // Bottom Left
 	 0.5f, -0.5f, 0.0f, // Bottom Right
-	 0.0f,  0.5f, 0.0f, // Up Center
+	 0.0f,  0.5f, 0.0f, // Top Center
 };
 
 int main()
