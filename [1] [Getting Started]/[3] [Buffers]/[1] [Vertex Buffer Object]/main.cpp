45833b0745d0ceac95ed441d4e6e52ba7fdf087a
Mohammed Khalid <77267002+glULTRA@users.noreply.github.com>
2021-08-12 15:03:49 +0300

    Add files via upload

---
+++ [1] [Getting Started]/[3] [Buffers]/[1] [Vertex Buffer Object]/main.cpp
@@ -0,0 +1,81 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+float vertices[] = 
+{ 
+	/* Position */
+	-0.5f, -0.5f, 0.0f, // Bottom Left
+	 0.5f, -0.5f, 0.0f, // Bottom Right
+	 0.0f,  0.5f, 0.0f, // Up Center
+};
+
+int main()
+{
+	/* Initialize GLFW */
+	glfwInit();
+
+	/* Initialize Version 3.3 */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// Create a window
+	GLFWwindow* window = glfwCreateWindow(1200, 800, "OpenGL 3.3", NULL, NULL);
+
+	// Check window
+	if (!window)
+	{
+		std::cout << "Failed to create a window\n";
+		glfwTerminate();
+	}
+
+	/* The most important part */
+	glfwMakeContextCurrent(window);
+
+	// Check Glew
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize Glew\n";
+		glfwTerminate();
+	}
+
+	/* Buffers */
+	unsigned int VBO;
+
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+	/* Position Attribute */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	/* Game Loop */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Update */
+
+		/* Render */
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f); // 0.0f - 1.0f
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		// Display 
+		glfwSwapBuffers(window);
+
+		// Pollevents
+		glfwPollEvents();
+	}
+
+	/* Clear */
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
