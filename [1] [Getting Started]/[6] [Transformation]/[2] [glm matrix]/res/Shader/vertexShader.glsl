b8be379e0430306007fa80d373be718ab1fbcd35
Mohammed Khalid <77267002+glULTRA@users.noreply.github.com>
2021-08-12 19:21:32 +0300

    Add files via upload

---
+++ [1] [Getting Started]/[6] [Transformation]/[2] [glm matrix]/res/Shader/vertexShader.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+out vec3 ourColor;
+
+uniform mat4 model;
+
+void main()
+{
+	ourColor = aColor;
+	gl_Position = model * vec4(aPos , 1.0f);
+}
