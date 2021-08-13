316c768233d727dfddd716eb85e7e0e81bebdd19
Mohammed Khalid <77267002+glULTRA@users.noreply.github.com>
2021-08-13 14:36:13 +0300

    Add files via upload

---
+++ [4] [Model Loading]/[2] [Model]/[3] [Sponza Model]/main.cpp
@@ -0,0 +1,387 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cmath>
+
+#include "Shader.h"
+#include "Camera.hpp"
+#include "Model.hpp"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+#include "stb_image.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+
+#define SCR_WIDTH 1000
+#define SCR_HEIGHT 800
+#define MAPSIZE_X 10
+#define MAPSIZE_Y 10
+#define MAPSIZE_Z 10
+#define GLSL_VERSION "#version 330"
+
+float vertices[] =
+{
+	/* Top Position */		/* Color */				/* TexCoords */		/* Normals */
+	-0.5f,-0.5f, 0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			0.0f, 0.0f, 1.0f,
+	 0.5f,-0.5f, 0.5f,		0.0f, 1.0f, 0.0f,		1.0f, 0.0f,			0.0f, 0.0f, 1.0f,
+	 0.5f, 0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			0.0f, 0.0f, 1.0f,
+	 0.5f, 0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			0.0f, 0.0f, 1.0f,
+	-0.5f, 0.5f, 0.5f,		1.0f, 1.0f, 0.0f,		0.0f, 1.0f,			0.0f, 0.0f, 1.0f,
+	-0.5f,-0.5f, 0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			0.0f, 0.0f, 1.0f,
+	/* Bottom Position */	/* Color */				/* TexCoords */		/* Normals */
+	-0.5f,-0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			0.0f, 0.0f,-1.0f,
+	 0.5f,-0.5f,-0.5f,		0.0f, 1.0f, 0.0f,		1.0f, 0.0f,			0.0f, 0.0f,-1.0f,
+	 0.5f, 0.5f,-0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			0.0f, 0.0f,-1.0f,
+	 0.5f, 0.5f,-0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			0.0f, 0.0f,-1.0f,
+	-0.5f, 0.5f,-0.5f,		1.0f, 1.0f, 0.0f,		0.0f, 1.0f,			0.0f, 0.0f,-1.0f,
+	-0.5f,-0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			0.0f, 0.0f,-1.0f,
+	/* Left Position */		/* Color */				/* TexCoords */	    /* Normals */
+	-0.5f,-0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,		   -1.0f, 0.0f, 0.0f,
+	-0.5f, 0.5f,-0.5f,		0.0f, 1.0f, 0.0f,		1.0f, 0.0f,		   -1.0f, 0.0f, 0.0f,
+	-0.5f, 0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,		   -1.0f, 0.0f, 0.0f,
+	-0.5f, 0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,		   -1.0f, 0.0f, 0.0f,
+	-0.5f,-0.5f, 0.5f,		1.0f, 1.0f, 0.0f,		0.0f, 1.0f,		   -1.0f, 0.0f, 0.0f,
+	-0.5f,-0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,		   -1.0f, 0.0f, 0.0f,
+	/* Right Position */	/* Color */				/* TexCoords */	    /* Normals */
+	 0.5f,-0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			1.0f, 0.0f, 0.0f,
+	 0.5f, 0.5f,-0.5f,		0.0f, 1.0f, 0.0f,		1.0f, 0.0f,			1.0f, 0.0f, 0.0f,
+	 0.5f, 0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			1.0f, 0.0f, 0.0f,
+	 0.5f, 0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			1.0f, 0.0f, 0.0f,
+	 0.5f,-0.5f, 0.5f,		1.0f, 1.0f, 0.0f,		0.0f, 1.0f,			1.0f, 0.0f, 0.0f,
+	 0.5f,-0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			1.0f, 0.0f, 0.0f,
+	 /* Back Position */	/* Color */				/* TexCoords */		/* Normals */
+	-0.5f, 0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			0.0f, 1.0f, 0.0f,
+	 0.5f, 0.5f,-0.5f,		0.0f, 1.0f, 0.0f,		1.0f, 0.0f,			0.0f, 1.0f, 0.0f,
+	 0.5f, 0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			0.0f, 1.0f, 0.0f,
+	 0.5f, 0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			0.0f, 1.0f, 0.0f,
+	-0.5f, 0.5f, 0.5f,		1.0f, 1.0f, 0.0f,		0.0f, 1.0f,			0.0f, 1.0f, 0.0f,
+	-0.5f, 0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f, 		0.0f, 1.0f, 0.0f,
+	/* Front Position */	/* Color */				/* TexCoords */		/* Normals */
+	-0.5f,-0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			0.0f,-1.0f, 0.0f,
+	 0.5f,-0.5f,-0.5f,		0.0f, 1.0f, 0.0f,		1.0f, 0.0f,			0.0f,-1.0f, 0.0f,
+	 0.5f,-0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			0.0f,-1.0f, 0.0f,
+	 0.5f,-0.5f, 0.5f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,			0.0f,-1.0f, 0.0f,
+	-0.5f,-0.5f, 0.5f,		1.0f, 1.0f, 0.0f,		0.0f, 1.0f,			0.0f,-1.0f, 0.0f,
+	-0.5f,-0.5f,-0.5f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,			0.0f,-1.0f, 0.0f,
+
+};
+
+/* Functions */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); // Protype
+void userInput(GLFWwindow* window); // protype
+void mouse_cursor_position(GLFWwindow* window, double xpos, double ypos); // Protype
+void mouse_scroll_position(GLFWwindow* window, double xoffset, double yoffset); // Protype
+
+unsigned int load_texture(const char* texture_path); // Protype
+
+glm::vec3 cubePositions[] =
+{
+	glm::vec3(0.0f, 0.0f, 0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
+glm::vec3 pointLightPositions[] =
+{
+	glm::vec3(0.7f,  0.2f,  2.0f),
+	glm::vec3(2.3f, -3.3f, -4.0f),
+	glm::vec3(-4.0f,  2.0f, -12.0f),
+	glm::vec3(0.0f,  0.0f, -3.0f)
+};
+
+/* Matrices */
+glm::mat4 model;
+glm::mat4 projection;
+glm::mat4 view;
+
+/* Light */
+glm::vec3 lightPos = glm::vec3(1.2f, 1.0f, 1.5f);
+
+
+/* Frames */
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+/* Camera */
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = float(SCR_WIDTH) / 2.0f;
+float lastY = float(SCR_HEIGHT) / 2.0f;
+bool isFirstMouse = true;
+
+int main()
+{
+	/* Initialize GLFW */
+	glfwInit();
+
+	/* Initialize Version 3.3 */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	/* Create a window */
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL 3.3", NULL, NULL);
+
+	// Check window
+	if (!window)
+	{
+		std::cout << "Failed to Create a window\n";
+		glfwTerminate();
+	}
+
+	/* The most important part */
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_cursor_position);
+	glfwSetScrollCallback(window, mouse_scroll_position);
+	//glfwSetInputMode(window, GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+
+	// check Glew 
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize Glew\n";
+		glfwTerminate();
+	}
+
+	/* Options */
+	glEnable(GL_DEPTH_TEST);
+
+	/* ImGui */
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	// Setup the Platform
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(GLSL_VERSION);
+
+	// Setup style
+	ImGui::StyleColorsDark();
+
+
+	/* Cube Buffers */
+	unsigned int VBO, VAO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+	// Position Attribute 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Color Attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Texture Attribute 
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// Normals Attribute
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(8 * sizeof(float)));
+	glEnableVertexAttribArray(3);
+
+	/* Light Buffers */
+	GLuint lightCubeVBO, lightCubeVAO;
+
+	glGenVertexArrays(1, &lightCubeVAO);
+	glGenBuffers(1, &lightCubeVBO);
+
+	glBindVertexArray(lightCubeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, lightCubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+	/* Light Position Attribute */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	/* Texture */
+	stbi_set_flip_vertically_on_load(true);
+
+	/* Model */
+	Model myBackPack("res/Object/Sponza/sponza.obj");
+
+	/* Shader */
+	Shader myShader("res/Shader/vertexShader.glsl", "res/Shader/fragmentShader.glsl");
+
+
+
+	// Some window
+	bool ShowDemo = false;
+	bool mySecondWindow = false;
+
+	/* Game Loop */
+	while (!glfwWindowShouldClose(window))
+	{
+
+		/* Update */
+		userInput(window);
+		float time = glfwGetTime();
+		deltaTime = time - lastFrame;
+		lastFrame = time;
+		float xValue = std::cos(time) / 2.0f + 0.5f; // 0.0f - 1.0f
+		float yValue = std::cos(time) / 2.0f + 0.5f; // 0.0f - 1.0f
+		float zValue = std::sin(time) / 2.0f + 0.5f; // 0.0f - 1.0f
+
+		float radius = 3.0f;
+		float camX = std::sin(time) * radius;
+		float camZ = std::cos(time) * radius;
+
+		// Gui Variables
+
+
+
+		/* Render */
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f); // 0.0 - 1.0
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		/* Start new frame for Dear ImGui */
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// First Object
+		myShader.use();
+		/* Coordinates */
+		// Projection
+		projection = glm::perspective(glm::radians(camera.Zoom), float(SCR_WIDTH) / float(SCR_HEIGHT), 0.1f, 100.0f);
+		myShader.setMat4("projection", projection);
+
+		// View
+		view = glm::mat4(1.0f);
+		view = camera.GetViewMatrix();
+		myShader.setMat4("view", view);
+
+		// Model
+		model = glm::mat4(1.0f);
+		myShader.setMat4("model", model);
+
+		// Draw your model
+		myBackPack.Draw(myShader);
+
+		// Render to screen
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		//Display
+		glfwSwapBuffers(window);
+		//Pollevents
+		glfwPollEvents();
+	}
+
+	/* Clear */
+	ImGui_ImplGlfw_Shutdown();
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui::DestroyContext();
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void userInput(GLFWwindow* window)
+{
+	const float camera_speed = 2.5f * deltaTime;
+
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) // To exit the program
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_TRUE) // Forward
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_TRUE) // Backward
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_TRUE) // Right
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_TRUE) // Left
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_TRUE) // Up
+		camera.ProcessKeyboard(UP, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_TRUE) // Down
+		camera.ProcessKeyboard(DOWN, deltaTime);
+
+}
+void mouse_cursor_position(GLFWwindow* window, double xpos, double ypos)
+{
+	if (isFirstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		isFirstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_TRUE)
+		camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void mouse_scroll_position(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
+
+unsigned int load_texture(const char* texture_path)
+{
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	/* Filter Options */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load(texture_path, &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		GLenum format;
+		if (nrChannels == 1)
+			format = GL_RED;
+		if (nrChannels == 3) // jpg file
+			format = GL_RGB;
+		if (nrChannels == 4) // png file
+			format = GL_RGBA;
+
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture\n";
+	}
+
+	stbi_image_free(data);
+
+	return texture;
+}
+
---
+++ [4] [Model Loading]/[2] [Model]/[3] [Sponza Model]/res/Shader/fragmentShader.glsl
@@ -0,0 +1,12 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D texture_diffuse;
+
+void main()
+{
+	FragColor = texture(texture_diffuse, TexCoords);
+}
