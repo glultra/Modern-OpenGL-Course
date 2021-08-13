c5fa584b20ae69985e801fce56b9a89fa473dac2
Mohammed Khalid <77267002+glULTRA@users.noreply.github.com>
2021-08-13 13:46:48 +0300

    Add files via upload

---
+++ [3] [Lightings]/[3] [Materials]/[3] [Material and Light GUI]/res/Shader/fragmentShader.glsl
@@ -0,0 +1,49 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 TexCoords;
+in vec3 ourColor;
+in vec3 normal;
+in vec3 fragPos;
+
+struct Material{
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+
+	float shininess;
+};
+
+struct Light{
+	vec3 position;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+uniform vec3 viewPos;
+
+void main()
+{
+	// Ambient
+	vec3 ambient = light.ambient * material.ambient;
+
+	// Diffuse
+	vec3 norm = normalize(normal);
+	vec3 lightDir = normalize(light.position-fragPos);
+	float diff = max(dot(norm,lightDir),0.0f);
+	vec3 diffuse = light.diffuse * (diff * material.diffuse);
+
+	// Specular
+	vec3 viewDir = normalize(viewPos - fragPos);
+	vec3 reflectDir = reflect(-lightDir,norm);
+	float spec = pow(max(dot(viewDir,reflectDir),0.0f), material.shininess);
+	vec3 specular = light.specular * (spec * material.specular);
+
+	vec3 result = ambient + diffuse + specular;
+	FragColor = vec4(result,1.0f);
+}
