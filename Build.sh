defaa5a49c4c925c39477e73eeeb81e52aa9a296
Mohammed Khalid <77267002+glULTRA@users.noreply.github.com>
2021-11-26 14:21:54 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -8,6 +8,7 @@ sudo apt-get install -y libxcursor-dev;
 sudo apt-get install -y libxi-dev;
 sudo apt-get -y update;
 sudo apt-get install -y libglfw3 libglfw3-dev;
+sudo apt-get install -y git;
 git clone https://github.com/glULTRA/OpenGL-Cross-Platform.git;
 cd OpenGL-Cross-Platform/;
 cmake -S . -B ./build/ -G "Unix Makefiles" -D GLFW_BUILD_DOCS=OFF -D BUILD_ASSIMP=ON;
